31975469a5630beba94a370ba94d78a6da359b15
Lukas <lukaspaulau@gmail.com>
2024-01-03 11:43:42 +0200

    data set visualisation

--- utils/blend_utilities.py
+++ utils/blend_utilities.py
@@ -1,4 +1,4 @@
-def CollectLocations2(data):
+""" def CollectLocations2(data):
     x = []
     y = []
     z = []
@@ -8,7 +8,8 @@ def CollectLocations2(data):
         y.append(data[2+(i*6)+1])
         z.append(data[2+(i*6)+2])
     locations = [x, y, z]
-    return locations
+    return locations """
+import bpy
 
 def CollectLocations2(data):
     locations = []
@@ -16,3 +17,40 @@ def CollectLocations2(data):
         location = (data[2+i*6], data[2+(i*6)+1], data[2+(i*6)+2])
         locations.append(location)
     return locations
+
+def CollectionExists(name):
+    for collection in bpy.data.collections:
+        if collection.name == name:
+            return True
+    return False
+
+def ObjectExists(name):
+    for object in bpy.data.objects:
+        if object.name == name:
+            return True
+    return False
+
+def HandLabel(data):
+    if data[1] == 1:
+        return "left"
+    return "right"
+
+def PoseLabel(data):
+    match data[0]:
+        case 0:
+            return "thumbs_up"
+        case 1:
+            return "thumbs_down"
+        case 2:
+            return "ok"
+        case 3:
+            return "victory"
+        case 4:
+            return "horns"
+        case 5:
+            return "phone"
+        case 6:
+            return "one"
+        case 7:
+            return "point"
+    return None
--- utils/blend_visualiseData.py
+++ utils/blend_visualiseData.py
@@ -2,9 +2,9 @@ import bpy
 import bmesh
 import os
 from parseData import ReadData
-from utilities import CollectLocations2
+import blend_utilities as bu
 
-path = os.path.abspath("D:\python-workspaces\deep-learning-tests\data")
+path = os.path.abspath("D:\Repositories\python-workspaces\deep-learning-tests\data")
 #path = "../data/"
 all_data = ReadData(path, loc=True)
 points = [[0, 1],
@@ -15,33 +15,42 @@ points = [[0, 1],
           [1, 15, 16, 17, 18, 23]]
 
 def CreateVerts(data, points):
+    c = 0
+    for data in all_data:
+        loc = bu.CollectLocations2(data)
+        #creates a new collection for a hand pose class (if it does not exist)
+        poseLabel = bu.PoseLabel(data)
+        if not bu.CollectionExists(poseLabel):
+            new_collection = bpy.data.collections.new(poseLabel)
+            bpy.context.scene.collection.children.link(new_collection)
+        collection = bpy.context.scene.collection.children[poseLabel]
+        #creats a new mesh object for a hand pose (if it does not exist)
+        hand = bu.HandLabel(data)
+        if not bu.ObjectExists("{}-{}-hand".format(c, hand)):    
+            mesh = bpy.data.meshes.new("mesh{}".format(c))
+            obj = bpy.data.objects.new("{}-{}-hand".format(c, hand), mesh)
+            collection.objects.link(obj)
+        obj = bpy.data.objects["{}-{}-hand".format(c, hand)]
+        
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+
+        mesh = bpy.context.object.data
+        bm = bmesh.new()
+
+        for v in loc:
+            v = [i * 0.1 for i in v]
+            bm.verts.new(v)
+
+        bm.verts.ensure_lookup_table()
+
+        for p in points:
+            for i in range(len(p)-1):
+                bm.edges.new((bm.verts[p[i]], bm.verts[p[i+1]]))
     
-    loc = CollectLocations2(data)
-
-    mesh = bpy.data.meshes.new("mesh")
-    obj = bpy.data.objects.new("Object", mesh)
-
-    #bpy.data.scenes[0].collection.objects.link(obj)
-    scene = bpy.context.scene
-    scene.collection.objects.link(obj)
-    bpy.context.view_layer.objects.active = obj
-    obj.select_set(True)
-
-    mesh = bpy.context.object.data
-    bm = bmesh.new()
-
-    for v in loc:
-        v = [i * 0.1 for i in v]
-        bm.verts.new(v)
-
-    bm.verts.ensure_lookup_table()
-
-    for p in points:
-        for i in range(len(p)-1):
-            bm.edges.new((bm.verts[p[i]], bm.verts[p[i+1]]))
-    
-    bm.to_mesh(mesh)
-    bm.free()
+        bm.to_mesh(mesh)
+        bm.free()
+        c = c + 1
     return
 
-CreateVerts(all_data[0], points)
+CreateVerts(all_data, points)
